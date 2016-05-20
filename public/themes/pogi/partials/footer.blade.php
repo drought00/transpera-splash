39d43f5355f25eb349071bafb905293c325cd20c
Adrian Sicat <john.sicat@republisys.com>
2016-05-20 15:24:16 +0800

    links

--- public/themes/pogi/partials/footer.blade.php
+++ public/themes/pogi/partials/footer.blade.php
@@ -3,13 +3,13 @@
         <div class="footerContents">
             <p class="footerPar">Follow us for updates</p>
             <p class="footerSocials">
-                <a class="socialLinks" href="#">
+                <a class="socialLinks" href="https://facebook.com/FonebayadPH" target="_blank">
                     <img class="socialImg" src="{{URL::to('/')}}/themes/pogi/assets/img/facebook.png"/>
                 </a>
-                <a class="socialLinks" href="#">
+                <a class="socialLinks" href="https://www.linkedin.com/company/transpera-inc." target="_blank">
                     <img class="socialImg" src="{{URL::to('/')}}/themes/pogi/assets/img/linkedIn.png"/>
                 </a>
-                <a class="socialLinks" href="#">
+                <a class="socialLinks" href="https://twitter.com/FonebayadPh" target="_blank">
                     <img class="socialImg" src="{{URL::to('/')}}/themes/pogi/assets/img/twitter.png"/>
                 </a>
             </p>
