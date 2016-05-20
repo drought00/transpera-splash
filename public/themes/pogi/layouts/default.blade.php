6c640eeca8e95f9985be1989ce2d465a998db9b5
Adrian Sicat <john.sicat@republisys.com>
2016-05-20 12:49:53 +0800

    mobile responsive

--- public/themes/pogi/layouts/default.blade.php
+++ public/themes/pogi/layouts/default.blade.php
@@ -3,6 +3,7 @@
     <head>
         <title>{{ Theme::get('title') }}</title>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="keywords" content="{{ Theme::get('keywords') }}">
         <meta name="description" content="{{ Theme::get('description') }}">
         {{ Theme::asset()->styles() }}
